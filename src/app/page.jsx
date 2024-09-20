99d7e22d5e65485f3bfb8111b313feb112497a27
Jonathan Benn <bonjennprojects@gmail.com>
2024-09-20 14:56:58 -0700

    Made text on hero 2 smaller

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -89,10 +89,10 @@ export default function Home() {
             <div className={styles.hero_2}>
               <h1 className={styles.enhancedText}>
                 <span style={{ fontWeight: '100' }}>Enhancing&nbsp;</span>
-                <span className={styles.highlight}> brand presence </span>
+                <span className={styles.highlight}> brand presence </span>&nbsp;
                 <span style={{ fontWeight: '100' }}> through&nbsp;</span>
                 <span className={styles.highlight}> artistic design&nbsp;</span>
-                <span style={{ fontWeight: '100' }}> and </span>
+                <span style={{ fontWeight: '100' }}> and </span>&nbsp;
                 <span className={styles.highlight}> digital products</span>.
               </h1>
             </div>
