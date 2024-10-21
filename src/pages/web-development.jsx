febf888a964453c819a70f1b18fb09a566c3e441
Jonathan Benn <bonjennprojects@gmail.com>
2024-10-21 16:53:49 -0700

    Added header and footer to web development page

--- src/pages/web-development.jsx
+++ src/pages/web-development.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import Hero from '../components/Hero';
 import ServicesPricing from '../components/services/servicesPricing';
 import ServicesTestimonials from '../components/services/servicesTestimonials';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function WebDevelopmentPage() {
   return (
     <div>
+      <Header />
       <Hero
         title="Get unlimited Web development for all your creative ideas"
         subtitle="Your own team of certified Web developers without the in-house hiring. We bring your design from idea to a pixel-perfect website."
@@ -14,6 +17,7 @@ export default function WebDevelopmentPage() {
       />
       <ServicesPricing />
       <ServicesTestimonials />
+      <Footer />
     </div>
   );
 }
