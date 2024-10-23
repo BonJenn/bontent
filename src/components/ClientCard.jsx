67b3f78a58c97248981ffb577500ecd30363b2dc
Jonathan Benn <bonjennprojects@gmail.com>
2024-10-22 21:40:25 -0700

    Fixed more compliling errors

--- src/components/ClientCard.jsx
+++ src/components/ClientCard.jsx
@@ -18,13 +18,14 @@ export default function ClientCard({ logo, name, description, width, height, bac
 			{ threshold: 0.1 }
 		);
 
-		if (cardRef.current) {
-			observer.observe(cardRef.current);
+		const currentRef = cardRef.current;
+		if (currentRef) {
+			observer.observe(currentRef);
 		}
 
 		return () => {
-			if (cardRef.current) {
-				observer.unobserve(cardRef.current);
+			if (currentRef) {
+				observer.unobserve(currentRef);
 			}
 		};
 	}, []);
