1247907a1d8d8dd97c206c9f81cd993103ae78a1
Jonathan Benn <bonjennprojects@gmail.com>
2024-10-23 15:19:30 -0700

    updating

--- src/components/services/servicesTestimonials.jsx
+++ src/components/services/servicesTestimonials.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import styles from '../../styles/services/servicesTestimonials.module.css';
 // ServicesTestimonials Component
 function ServicesTestimonials() {
     return (
