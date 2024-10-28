3f750cccb0bc777fd88bb0b64f735d896e4b4200
Jonathan Benn <bonjennprojects@gmail.com>
2024-10-28 11:42:32 -0700

    Redesigned tetimonials

--- src/components/services/servicesTestimonials.jsx
+++ src/components/services/servicesTestimonials.jsx
@@ -3,10 +3,10 @@ import styles from '../../styles/services/servicesTestimonials.module.css';
 // ServicesTestimonials Component
 function ServicesTestimonials() {
     return (
-        <div className="services-testimonials">
+        <div className={styles.servicesTestimonials}>
             {/* Testimonial 1 */}
-            <div className="testimonial-container">
-                <div className="testimonial">
+            <div className={styles.testimonialContainer}>
+                <div className={styles.testimonial}>
                     <p><strong>Name:</strong> Todd Matthews</p>
                     <p><strong>Company:</strong> BitcoinBrainiac.org</p>
                     <p><strong>Testimonial:</strong> Bontent&apos;s web development services have been a game-changer for us. Their team delivered a sleek, user-friendly website that has significantly improved our user engagement. Their attention to detail and commitment to excellence is unmatched.</p>
@@ -14,8 +14,8 @@ function ServicesTestimonials() {
             </div>
 
             {/* Testimonial 2 */}
-            <div className="testimonial-container">
-                <div className="testimonial">
+            <div className={styles.testimonialContainer}>
+                <div className={styles.testimonial}>
                     <p><strong>Name:</strong> John Benn</p>
                     <p><strong>Company:</strong> Public Assemblage Group</p>
                     <p><strong>Testimonial:</strong> Working with Bontent was a seamless experience. They understood our needs perfectly and delivered a website that exceeded our expectations. Their professionalism and expertise are truly commendable.</p>
@@ -23,8 +23,8 @@ function ServicesTestimonials() {
             </div>
 
             {/* Testimonial 3 */}
-            <div className="testimonial-container">
-                <div className="testimonial">
+            <div className={styles.testimonialContainer}>
+                <div className={styles.testimonial}>
                     <p><strong>Name:</strong> Erica Slater</p>
                     <p><strong>Company:</strong> Vistaview Technical Solutions</p>
                     <p><strong>Testimonial:</strong> Bontent transformed our online presence with their exceptional web development services. The new site is not only visually appealing but also highly functional. We couldn&apos;t be happier with the results.</p>
