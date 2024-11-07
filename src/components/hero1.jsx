fc5d1bd833a9095c548eb29258e3a0d75fab9fd1
Jonathan Benn <bonjennprojects@gmail.com>
2024-11-06 21:40:03 -0800

    Fixed video on mobile

--- src/components/hero1.jsx
+++ src/components/hero1.jsx
@@ -5,7 +5,15 @@ export default function Hero1() {
   return (
     <div className={styles.hero_1}>
       <div className={`${styles.videoContainer} ${styles.overlayContainer}`}>
-        <video className={styles.video} autoPlay loop muted>
+        <video 
+          className={styles.video} 
+          autoPlay 
+          loop 
+          muted 
+          playsInline
+          preload="auto"
+          poster="/images/video-poster.jpg"
+        >
           <source src="/images/bontent_animation.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
